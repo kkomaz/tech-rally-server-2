fbeb1c99000f5bf77558f6451375088ea2d0dba7
Alex Lee <alex@uniteus.com>
2020-04-01 20:42:59 -0400

    AWS image upload complete with service and dummy route

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+  AWS_SECRET_ACCESS: process.env.AWS_SECRET_ACCESS,
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const product = require('./product.route');
+const fileRoutes = require('./file-upload');
 
 module.exports = (server) => {
+  server.use('/api', fileRoutes);
   server.use('/api/products', product);
 };
