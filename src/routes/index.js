2bb96027651439d4c9dfceec3735825f0b6c2ae8
Alex Lee <alex@uniteus.com>
2020-04-06 09:19:49 -0400

    Remove file upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const blogs = require('./blog.route');
 const product = require('./product.route');
-const fileRoutes = require('./file-upload');
 
 module.exports = (server) => {
-  server.use('/api', fileRoutes);
   server.use('/api/products', product);
   server.use('/api/blogs', blogs);
 };
