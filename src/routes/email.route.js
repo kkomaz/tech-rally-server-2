b688f02680cdc2a34d4e74437966be486fd63a80
Alex Lee <alex@uniteus.com>
2020-04-29 01:35:24 -0400

    Add emailing

--- src/routes/email.route.js
+++ src/routes/email.route.js
@@ -18,25 +18,51 @@ router.post('/send-contact-2', (req, res, next) => {
 });
 
 router.post('/send-contact', (req, res, next) => {
-  const { body: { email, name, message } } = req;
+  const { body: { email, name, message, mailing } } = req;
 
-  Email.findOne({ email }, (err, emailObj) => {
-    if (err) return next(err);
+  const mailOptions = {
+    from: config.EMAIL,
+    to: config.TO_EMAIL,
+    subject: `Email sent via techrally.me ${name}, ${email}`,
+    text: message,
+  };
 
-    const mailOptions = {
-      from: config.EMAIL,
-      to: config.TO_EMAIL,
-      subject: `Email sent via techrally.me ${name}, ${email}`,
-      text: message,
-    };
+  if (!mailing) {
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        res.status(error.responseCode).send({
+          status: error.responseCode,
+          text: 'Something went wrong!'
+        });
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.send({ success: true });
+      }
+    });
+  } else {
+    Email.findOne({ email }, (err, emailObj) => {
+      if (err) return next(err);
 
-    if (!emailObj) {
-      const newEmail = new Email({
-        email,
-        name,
-      });
+      if (!emailObj) {
+        const newEmail = new Email({
+          email,
+          name,
+        });
 
-      newEmail.save((err) => {
+        newEmail.save((err) => {
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              res.status(error.responseCode).send({
+                status: error.responseCode,
+                text: 'Something went wrong!'
+              });
+            } else {
+              console.log('Email sent: ' + info.response);
+              res.send({ success: true });
+            }
+          });
+        });
+      } else {
         transporter.sendMail(mailOptions, function(error, info) {
           if (error) {
             res.status(error.responseCode).send({
@@ -48,21 +74,9 @@ router.post('/send-contact', (req, res, next) => {
             res.send({ success: true });
           }
         });
-      });
-    } else {
-      transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-          res.status(error.responseCode).send({
-            status: error.responseCode,
-            text: 'Something went wrong!'
-          });
-        } else {
-          console.log('Email sent: ' + info.response);
-          res.send({ success: true });
-        }
-      });
-    }
-  });
+      }
+    }); 
+  }
 });
 
 module.exports = router;
